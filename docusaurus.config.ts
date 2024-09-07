102fd046019982160b6444d57448bd631a9a66a3
StructureBox <structure.box@gmail.com>
2024-09-07 14:31:01 +0900

    fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   url: "https://structurebox.tech",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/docs-structurebox",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
