a79a651c76a64a6f0004060094e214080382b7d1
StructureBox <structure.box@gmail.com>
2024-09-07 13:11:31 +0900

    fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://structurebox.tech",
+  url: "https://structurebox.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -15,7 +15,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "structurebox", // Usually your GitHub org/user name.
-  projectName: "docs-structurebox",
+  projectName: "structurebox.github.io",
   trailingSlash: false, // Usually your repo name.
 
   onBrokenLinks: "throw",
